51406ab8130ed8caa83283b7f11a14f7a5c19631
cdray527 <cdray085@hotmail.com>
2024-09-25 16:59:42 +0800

    Updated .gitignore

--- apps/store/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"pages/module-federation/component/index.tsx -> checkout/buy-button\":{\"id\":\"pages/module-federation/component/index.tsx -> checkout/buy-button\",\"files\":[]}}"
